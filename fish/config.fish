a41837ca8abc8e5eb31586c4504da2fabfeeea74
abhirup-m <mukherjeeabhirup44@gmail.com>
2025-10-16 01:48:25 +0530

    added fish shell config

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+fish_add_path /usr/local/bin
+fish_add_path $HOME/.local/bin
+set -gx EDITOR "nvim"
+set -gx VISUAL "nvim"
+set -gx JULIA_PKG_USE_CLI_GIT "true"
+set -gx QT_QPA_PLATFORMTHEME "qt6ct"
+set -gx MOZ_ENABLE_WAYLAND "1"
+set -gx QT_QPA_PLATFORM "wayland"
+set -gx SDL_VIDEODRIVER "wayland"
+set -gx XDG_SESSION_TYPE "wayland"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+
+if status --is-login
+	if test -z "$DISPLAY" -a $XDG_VTNR = 1
+		pulseaudio --start -D
+		exec dwl -s dwl-start.sh
+	end
+end
+
+set fish_greeting
