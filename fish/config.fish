4f334546ed37f15ff8b4ed917f7e29d30ee1bcce
abhirup-m <mukherjeeabhirup44@gmail.com>
2025-10-16 01:54:35 +0530

    added some aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 fish_add_path /usr/local/bin
 fish_add_path $HOME/.local/bin
+
+#### ENV VARIABLES ####
 set -gx EDITOR "nvim"
 set -gx VISUAL "nvim"
 set -gx JULIA_PKG_USE_CLI_GIT "true"
@@ -12,6 +14,12 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 
+#### ALIASES ####
+alias ls "tree -L 1 -C -h --du -a -D | column"
+alias mpython "$HOME/storage/miniconda3/bin/python"
+alias mpip "$HOME/storage/miniconda3/bin/pip"
+
+#### START WAYLAND COMPOSITOR ####
 if status --is-login
 	if test -z "$DISPLAY" -a $XDG_VTNR = 1
 		pulseaudio --start -D
